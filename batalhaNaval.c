d34e20611576879a66607bc29815eb770528c980
CatPy123 <catarinalima@sempreceub.com>
2025-06-23 16:02:27 +0000

    correções

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,9 +7,45 @@
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+
+    int batalha[10][10];
+
+    int navioh[3] = {3, 3, 3};
+
+    int naviov[3] = {3, 3, 3};
+
+    int linha_inicial_naviov = 1, coluna_inicial_naviov = 3, linha_inicial_navioh = 5, coluna_inicial_navioh = 3, k = 0, l = 0;
+
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j < 10; j++) {
+            printf("%d, %d", i, j);
+            if (i == linha_inicial_naviov && j == coluna_inicial_naviov) {
+                if (naviov[k] != 0) {
+                    printf(" %d ", naviov[k]);
+                    k++;
+                    linha_inicial_naviov++;
+                }
+            }
+            if (i == (linha_inicial_navioh) && j == (coluna_inicial_navioh)) {
+                if (navioh[l] != 0) {
+                    printf(" %d ", naviov[l]);
+                    l++;
+                    coluna_inicial_navioh++;
+                }
+            }
+            else if (i != (linha_inicial_naviov - 1) && j == coluna_inicial_naviov || i == (linha_inicial_navioh) && j == (coluna_inicial_navioh - 1)) {
+                printf(" 0 ");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+
+    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
