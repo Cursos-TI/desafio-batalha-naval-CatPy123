b8a21680e2e4f49324299b320ecd20b93e1cc426
CatPy123 <catarinalima@sempreceub.com>
2025-06-24 03:35:26 +0000

    Nível novato finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -8,42 +8,69 @@ int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
 
-    int batalha[10][10];
+    int batalha[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+    };
 
     int navioh[3] = {3, 3, 3};
 
     int naviov[3] = {3, 3, 3};
 
-    int linha_inicial_naviov = 1, coluna_inicial_naviov = 3, linha_inicial_navioh = 5, coluna_inicial_navioh = 3, k = 0, l = 0;
+    int linha_inicial_naviov = 8, coluna_inicial_naviov = 3, linha_inicial_navioh = 1, coluna_inicial_navioh = 3, k = 0, l = 0;
 
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+    if (linha_inicial_naviov >= 9 || coluna_inicial_naviov >= 11) {
+        printf("A coordenada inicial do navio vertical está fora dos limites do tabuleiro\n\n");
+    }
+    if (linha_inicial_navioh >= 11 || coluna_inicial_navioh >= 9){
+        printf("A coordenada inicial do navio horizontal está fora dos limites do tabuleiro\n\n");
+    }
 
     for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j < 10; j++) {
-            printf("%d, %d", i, j);
+        for (int j = 1; j <= 10; j++) {
             if (i == linha_inicial_naviov && j == coluna_inicial_naviov) {
                 if (naviov[k] != 0) {
-                    printf(" %d ", naviov[k]);
                     k++;
                     linha_inicial_naviov++;
+                    if (batalha[(i-1)][(j-1)] == 3) {
+                        printf("Os barcos estão se sobrepondo - altere a posição de um dos barcos\n\n");
+                    }
+                    batalha[(i-1)][(j-1)] = 3;
                 }
             }
             if (i == (linha_inicial_navioh) && j == (coluna_inicial_navioh)) {
                 if (navioh[l] != 0) {
-                    printf(" %d ", naviov[l]);
                     l++;
                     coluna_inicial_navioh++;
+                    if (l <= 3) {
+                        if (batalha[(i-1)][(j-1)] == 3) {
+                            printf("Os barcos estão se sobrepondo - altere a posição de um dos barcos\n");
+                        }
+                        batalha[(i-1)][(j-1)] = 3;
+                    }
                 }
             }
-            else if (i != (linha_inicial_naviov - 1) && j == coluna_inicial_naviov || i == (linha_inicial_navioh) && j == (coluna_inicial_navioh - 1)) {
-                printf(" 0 ");
-            }
+        }
+    }
+
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            printf(" %d ", batalha[(i-1)][(j-1)]);
         }
         printf("\n");
     }
-    printf("\n\n");
 
     
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
